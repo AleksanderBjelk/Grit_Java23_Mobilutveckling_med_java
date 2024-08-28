a42dd4d9b1a4fb999c962dd9a3a1679d7f7147e8
AleksanderBjelk <bjelkaleksander@gmail.com>
2024-08-28 10:13:24 +0200

    push 2

--- lektion 2/app/src/main/java/com/example/lektion2/MainActivity.java
+++ lektion 2/app/src/main/java/com/example/lektion2/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.lektion2;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -51,6 +54,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        ImageView iv = findViewById(R.id.imageViewDroid);
+
+        iv.setAlpha(0.2f);
+        iv.setRotationY(170);
+        iv.setImageResource(R.drawable.img_1033);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
