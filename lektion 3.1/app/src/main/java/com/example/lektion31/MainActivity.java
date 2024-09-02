36c41a1ca7986507f395c5960db4c4017b7b0f39
AleksanderBjelk <bjelkaleksander@gmail.com>
2024-09-02 11:10:34 +0200

    push 6 shared preference

--- lektion 3.1/app/src/main/java/com/example/lektion31/MainActivity.java
+++ lektion 3.1/app/src/main/java/com/example/lektion31/MainActivity.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +16,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    EditText et;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +35,22 @@ public class MainActivity extends AppCompatActivity {
         Log.d("Aleksander",sharedPref.getString("Key","default text if null"));
 
 
-        editor.putString("Key","Aleksander har bra vibes" );
-        editor.apply();
+
+
+        tv = findViewById(R.id.text2);
+        et = findViewById(R.id.editTextText);
+        btn = findViewById(R.id.button);
+        //när man startar appen så sötter den texten från sharedpref
+        tv.setText(sharedPref.getString("key", "[No text found]"));
+
+        btn.setOnClickListener((e)-> {
+            String inputtedText = et.getText().toString();
+            editor.putString("key", inputtedText).apply();;
+            Toast.makeText(MainActivity.this, "saved", Toast.LENGTH_SHORT).show();
+            tv.setText(inputtedText);
+        });
+
+
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
