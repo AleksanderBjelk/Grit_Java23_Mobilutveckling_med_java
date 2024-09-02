ec1754be4b176178e5381e42c6904ae53e58f520
AleksanderBjelk <bjelkaleksander@gmail.com>
2024-09-02 09:38:14 +0200

    Lektion 3 push 1 log

---
+++ lektion 3/app/src/main/java/com/example/lektion3/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.lektion3;
+
+import static android.content.ContentValues.TAG;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    //private static final String TAG = "Aleksander";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Log.d("Aleksander", "Hello");
+
+        //med TAG så har jag nu sparat TAG som Aleksander under hela projeketet (rad 14)
+        Log.i(TAG, "onCreate: ");
+        Log.d(TAG, "Hello: ");
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
