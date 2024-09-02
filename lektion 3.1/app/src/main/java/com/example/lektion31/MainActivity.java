2ba9360bead058081adaeb6a730d1388ae6c11d7
AleksanderBjelk <bjelkaleksander@gmail.com>
2024-09-02 10:36:30 +0200

    push 5 local storage

--- lektion 3.1/app/src/main/java/com/example/lektion31/MainActivity.java
+++ lektion 3.1/app/src/main/java/com/example/lektion31/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.lektion31;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences sharedPref = this.getSharedPreferences(
+                getString(R.string.preferance), MODE_PRIVATE);
+        SharedPreferences.Editor editor=sharedPref.edit();
+
+
+        Log.d("Aleksander",sharedPref.getString("Key","default text if null"));
+
+
+        editor.putString("Key","Aleksander har bra vibes" );
+        editor.apply();
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
